848aa31277268818155cbd83b2af4fadc2d5a536
ankitojha2026 <ojhaankit064@gmail.com>
2025-01-10 16:53:03 +0530

    I have completed Number is prime or not

--- src/BasicCoding/PrimeNumberOrNot.java
+++ src/BasicCoding/PrimeNumberOrNot.java
@@ -1,7 +1,27 @@
 package BasicCoding;
 
+import java.util.Scanner;
+
 public class PrimeNumberOrNot {
+    private static boolean isPrime(int num)
+    {
+      for(int i=2;i<num/2+1 ;i++)
+      {
+          if(num%i== 0)
+          {
+              return false;
+          }
+      }
+        return true;
+    }
     public static void main(String[] args) {
 
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number which you want to check:");
+        int num=sc.nextInt();
+        if(isPrime(num))
+            System.out.println("Number is Prime ");
+        else
+            System.out.println("Number is Not Prime ");
     }
 }
