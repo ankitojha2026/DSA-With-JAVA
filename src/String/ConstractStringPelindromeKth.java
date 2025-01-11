686a72bff33aa929d26d5ed592f64fbeb0eccc5b
ankitojha2026 <ojhaankit064@gmail.com>
2025-01-11 21:29:50 +0530

    Lettcode Question

---
+++ src/String/ConstractStringPelindromeKth.java
@@ -0,0 +1,20 @@
+package String;
+
+
+
+
+
+public class ConstractStringPelindromeKth {
+
+    public boolean canConstruct(String s, int k) {
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
