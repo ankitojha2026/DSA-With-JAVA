aa33e7391d36b064bf0e98e159e031ca98cd9152
ankitojha2026 <ojhaankit064@gmail.com>
2025-03-02 21:53:58 +0530

    new leetcode today 2530

---
+++ src/hashmap/MergeTwoArrayLeetCode2570.java
@@ -0,0 +1,46 @@
+package hashmap;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MergeTwoArrayLeetCode2570 {
+    public static void main(String[] args) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+
+        int [][]arr1={{1,2},{2,3},{4,5}};
+        int [][]arr2={{1,4},{3,2},{4,1}};
+
+        for (int i=0;i<arr1.length ;i++)
+        {
+            map.put(arr1[i][0],arr1[i][1]);
+        }
+
+        for (int i=0;i<arr2.length ;i++)
+        {
+            if(map.containsKey(arr2[i][0]))
+            {
+                map.put(arr2[i][0],map.get(arr2[i][0])+arr2[i][1]);
+            }
+            else
+            {
+                map.put(arr2[i][0],arr2[i][1]);
+            }
+
+        }
+
+        int [][]ans=new int[map.size()][2];
+
+        int j=0;
+        for (Map.Entry<Integer, Integer> i : map.entrySet())
+        {
+            ans[j][0]=i.getKey();
+            ans[j][1]=i.getValue();
+            j++;
+        }
+
+
+
+
+    }
+}
