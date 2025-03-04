7e5a3c4461372f31a41f16ccf97d9cc3cfe78af2
ankitojha2026 <ojhaankit064@gmail.com>
2025-03-04 11:02:35 +0530

    basic linked list by using inbuild class

---
+++ src/LinkedList/InbuildLinkeList.java
@@ -0,0 +1,19 @@
+package LinkedList;
+import java.util.LinkedList;
+public class InbuildLinkeList {
+    public static void main(String[] args) {
+
+        LinkedList<Integer>l=new LinkedList<>();
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        System.out.println(l);
+        l.remove(1);
+        System.out.println(l);
+
+
+
+    }
+}
