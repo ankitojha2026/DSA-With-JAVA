c0e7ca7a0b00222d0fb3d4978ffd83334b893f0c
ankitojha2026 <ojhaankit064@gmail.com>
2025-05-17 21:36:54 +0530

    next Premutation

---
+++ src/GFG160/Day5.java
@@ -0,0 +1,56 @@
+package GFG160;
+
+import java.util.Arrays;
+
+public class Day5 {
+  public static  void nextPermutation(int[] arr) {
+        // code here
+        int n=arr.length ;
+        int i=n-2;
+
+        while(i>=0 && arr[i]>=arr[i+1])
+        {
+            i--;
+
+        }
+
+        if(i>=0)
+        {
+            int j=n-1;
+            while(arr[j]<=arr[i])
+            {
+                j--;
+            }
+
+            swap(arr,i,j);
+        }
+
+        reverse(arr, i+1,n-1);
+
+    }
+
+    private static void swap(int arr[] , int i , int j)
+    {
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    private static void reverse(int []arr, int i ,int j)
+    {
+        while(i<j)
+        {
+            swap(arr,i,j);
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int []arr={3,1,5,1};
+        nextPermutation(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
